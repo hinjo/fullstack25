f6cc561e006312ed6fb120b8913ad7e2533bb19d
Vanja Ferhatovic <vanja.ferhatovic@gmail.com>
2025-05-30 09:30:22 +0200

    fix message bug - 1.3.1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   return (
     <>
       <div>
-        Heeej 🙌
+        Heeej 🙌🙌
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
